23d0424cad607dec157e45f5808b3a8c6780e8f0
Oleh Hrytsiv <g.r.i.t.s.i.v.o.l.e.g.95@gmail.com>
2022-07-12 02:08:37 +0300

    Update WhoAmIApplication.java

--- src/main/java/com/eleks/academy/whoami/WhoAmIApplication.java
+++ src/main/java/com/eleks/academy/whoami/WhoAmIApplication.java
@@ -1,22 +1,19 @@
 package com.eleks.academy.whoami;
 
-//import com.eleks.academy.whoami.api.GameApi;
-//import com.eleks.academy.whoami.handler.ApiClient;
-//import com.eleks.academy.whoami.api.DefaultApi;
-//import com.eleks.academy.whoami.api.PlayersOnlineApi;
+import com.eleks.academy.whoami.api.GameApi;
+import com.eleks.academy.whoami.handler.ApiClient;
+import com.eleks.academy.whoami.api.DefaultApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-/*@ComponentScan(basePackages = {"com.eleks.academy.whoami"}, excludeFilters = {
+@ComponentScan(basePackages = {"com.eleks.academy.whoami"}, excludeFilters = {
 		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ApiClient.class),
 		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = GameApi.class),
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DefaultApi.class),
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = PlayersOnlineApi.java
-		)
-})*/
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DefaultApi.class)
+})
 public class WhoAmIApplication {
 
 	public static void main(String[] args) {
