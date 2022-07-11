c7f445f9ce7d7bfeae4ba97f3169e3932e531ccb
Oleh Hrytsiv <g.r.i.t.s.i.v.o.l.e.g.95@gmail.com>
2022-07-12 02:11:18 +0300

    Update WhoAmIApplication.java

--- src/main/java/com/eleks/academy/whoami/WhoAmIApplication.java
+++ src/main/java/com/eleks/academy/whoami/WhoAmIApplication.java
@@ -2,7 +2,6 @@ package com.eleks.academy.whoami;
 
 import com.eleks.academy.whoami.api.GameApi;
 import com.eleks.academy.whoami.handler.ApiClient;
-import com.eleks.academy.whoami.api.DefaultApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,8 +10,7 @@ import org.springframework.context.annotation.FilterType;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.eleks.academy.whoami"}, excludeFilters = {
 		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ApiClient.class),
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = GameApi.class),
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DefaultApi.class)
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = GameApi.class)
 })
 public class WhoAmIApplication {
 
