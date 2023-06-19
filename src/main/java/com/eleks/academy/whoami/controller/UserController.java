3f054fe1422273b5f11c8d12f4dcdf79c2f0c8b5
Oleh Hrytsiv <g.r.i.t.s.i.v.o.l.e.g.95@gmail.com>
2023-06-19 12:12:20 +0100

    Update UserController.java

--- src/main/java/com/eleks/academy/whoami/controller/UserController.java
+++ src/main/java/com/eleks/academy/whoami/controller/UserController.java
@@ -1,56 +1,56 @@
-package com.eleks.academy.whoami.controller;
-
-import com.eleks.academy.whoami.model.request.AuthorisationRequest;
-import com.eleks.academy.whoami.model.request.UserRequestDto;
-import com.eleks.academy.whoami.model.response.UserResponseDto;
-import com.eleks.academy.whoami.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-
-@RestController
-@RequestMapping("/users")
-@RequiredArgsConstructor
-public class UserController {
-
-    private final UserService userService;
-
-    @PostMapping
-    public UserResponseDto create(@Valid @RequestBody UserRequestDto userRequestDto) {
-        return this.userService.create(userRequestDto);
-    }
-
-    @PostMapping("/authorisation")
-    public UserResponseDto authorisationUser(@RequestBody AuthorisationRequest authorisationRequest){
-        return this.userService.authorisationUser(authorisationRequest);
-    }
-
-    @GetMapping("/{userId}")
-    public ResponseEntity<UserResponseDto> get(@PathVariable("userId") Long userId) {
-        return ResponseEntity.ok(userService.get(userId));
-    }
-
-    @PutMapping("/{userId}")
-    public ResponseEntity<UserResponseDto> update(@PathVariable("userId") Long userId,@Valid @RequestBody UserRequestDto userRequestDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.update(userId, userRequestDto));
-    }
-
-    @DeleteMapping("/{userId}")
-    public void deleteById(@PathVariable("userId") Long userId) {
-        userService.deleteById(userId);
-    }
-
-    @GetMapping
-    public ResponseEntity<Page<UserResponseDto>> getAll(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
-                                                        @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
-        Pageable paging = PageRequest.of(page, size);
-        return ResponseEntity.ok(userService.getAll(paging));
-    }
-
-}
+//package com.eleks.academy.whoami.controller;
+//
+//import com.eleks.academy.whoami.model.request.AuthorisationRequest;
+//import com.eleks.academy.whoami.model.request.UserRequestDto;
+//import com.eleks.academy.whoami.model.response.UserResponseDto;
+//import com.eleks.academy.whoami.service.UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.validation.Valid;
+//
+//@RestController
+//@RequestMapping("/users")
+//@RequiredArgsConstructor
+//public class UserController {
+//
+//    private final UserService userService;
+//
+//    @PostMapping
+//    public UserResponseDto create(@Valid @RequestBody UserRequestDto userRequestDto) {
+//        return this.userService.create(userRequestDto);
+//    }
+//
+//    @PostMapping("/authorisation")
+//    public UserResponseDto authorisationUser(@RequestBody AuthorisationRequest authorisationRequest){
+//        return this.userService.authorisationUser(authorisationRequest);
+//    }
+//
+//    @GetMapping("/{userId}")
+//    public ResponseEntity<UserResponseDto> get(@PathVariable("userId") Long userId) {
+//        return ResponseEntity.ok(userService.get(userId));
+//    }
+//
+//    @PutMapping("/{userId}")
+//    public ResponseEntity<UserResponseDto> update(@PathVariable("userId") Long userId,@Valid @RequestBody UserRequestDto userRequestDto) {
+//        return ResponseEntity.status(HttpStatus.CREATED).body(userService.update(userId, userRequestDto));
+//    }
+//
+//    @DeleteMapping("/{userId}")
+//    public void deleteById(@PathVariable("userId") Long userId) {
+//        userService.deleteById(userId);
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<Page<UserResponseDto>> getAll(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
+//                                                        @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+//        Pageable paging = PageRequest.of(page, size);
+//        return ResponseEntity.ok(userService.getAll(paging));
+//    }
+//
+//}
